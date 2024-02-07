5a73df5994c02054e219a3e6644b3e21be16461a
Christine Poerschke <cpoerschke@apache.org>
2024-02-07 12:41:30 +0000

    tentative: add indexSearcherExecutorThreads element to solr.xml

--- solr/core/src/test/org/apache/solr/core/TestSolrXml.java
+++ solr/core/src/test/org/apache/solr/core/TestSolrXml.java
@@ -85,7 +85,8 @@ public class TestSolrXml extends SolrTestCaseJ4 {
     assertEquals("core sorter class", "testCoreSorter", cfg.getCoreSorterClass());
     assertEquals("core load threads", 11, cfg.getCoreLoadThreadCount(false));
     assertEquals("replay update threads", 100, cfg.getReplayUpdatesThreads());
-    assertThat(
+    assertEquals("index searcher executor threads", 7, cfg.getIndexSearcherExecutorThreads());
+    MatcherAssert.assertThat(
         "core root dir",
         cfg.getCoreRootDirectory().toString(),
         containsString("testCoreRootDirectory"));
